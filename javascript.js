c7117ccbbd1b109dbf3c07a6f8a7dda95f2aa8dd
Bennett Silverman <152738859+BSilverman6@users.noreply.github.com>
2023-12-23 17:07:11 -0700

    added javascript worked through evaluating individual expressions, only one decimal per entry, and (-) sign only when appropriate

--- javascript.js
+++ javascript.js
@@ -1,6 +1,136 @@
 const screen = document.querySelector(".screen");
-const typer = document.querySelectorAll(".num, oper");
+const number = document.querySelectorAll(".num");
+const oper = document.querySelectorAll(".oper");
+const restart = document.querySelector("#ac");
+const ekwals = document.querySelector("#ekwals");
+const negative = document.querySelector("#plusMin")
+let screenArray=[""];
 
-typer.forEach((item)=>{
-    item.addEventListener("click")
+
+
+number.forEach((item)=>{
+    item.addEventListener("click", (event)=>{
+        if (!isOper(lastItem())){
+            if(event.target.textContent == "." && lastItem().includes(".")){
+                //do nothing
+            }else{
+            screenArray[screenArray.length-1] += `${event.target.textContent}`;
+            }
+        }else if(screenArray.length>1&&lastItem()==="-"&&isOper(screenArray[screenArray.length-2])){
+            screenArray[screenArray.length-1] += `${event.target.textContent}`;
+        }else if(lastItem()==="-"&&screenArray.length===1){
+            screenArray[screenArray.length-1] += `${event.target.textContent}`;
+
+        }else{
+            screenArray.push(`${event.target.textContent}`);
+        }
+        setScreen();
+    });
 });
+
+negative.addEventListener("click", ()=>{
+    console.log("negative function: " + !isOper(lastItem()));
+    if (screenArray.length==1&&lastItem()===""){
+        screenArray[0]="-"
+    }else if(screenArray.length==1&&lastItem()==="-"){
+        screenArray[0] = ""
+    }else if (!isOper(lastItem())){
+        screenArray[screenArray.length-1] = ((+screenArray[screenArray.length-1])*(-1)).toString();
+    }else{
+        screenArray.push("-");
+    }
+    setScreen();
+});
+
+oper.forEach((item)=>{
+    item.addEventListener("click", (event)=>{
+        if (screen.textContent !== ""){
+            if (isOper(lastItem())){
+                screenArray[screenArray.length-1] = event.target.textContent;
+            }else{
+                screenArray.push(event.target.textContent);
+            }
+            setScreen();
+        }
+    });
+});
+
+restart.addEventListener("click", () => {
+    screenArray = [""];
+    setScreen();
+    screen.textContent = "0";
+});
+
+ekwals.addEventListener("click", ()=> compute(screenArray));
+
+function compute(mathArray){
+    const operands = Math.ceil((mathArray.length)/2)
+    console.log(operands);
+    if (operands >= 2){
+        mathWork: for (let i=1; i<operands; i++){
+            switch (screenArray[1]){
+                case "+":
+                    screenArray.splice(0,3,String(add(screenArray[0],screenArray[2])));
+                    setScreen();
+                    break;
+                case "-":
+                    screenArray.splice(0,3,String(subtract(screenArray[0],screenArray[2])));
+                    setScreen();
+                    break;
+                case "x":
+                    screenArray.splice(0,3,String(multiply(screenArray[0],screenArray[2])));
+                    setScreen();
+                    break;
+                case "/":
+                    if (screenArray[2] == "0"){
+                        screenArray = [""];
+                        setScreen();
+                        screen.textContent = "Well Poopies...";
+                        break mathWork;
+                    }else{
+                    screenArray.splice(0,3,String(divide(screenArray[0],screenArray[2])));
+                    setScreen();
+                    }
+                    break;
+            }
+        }
+    }
+};
+
+function setScreen(){
+    screen.textContent=screenArray.toString("").replaceAll(",","");
+    console.log(screenArray);
+}
+
+function isOper(z){
+    if (z === "/"||z === "+"||z === "-"||z === "x"){
+        return true;
+    }
+    return false;;
+}
+
+function lastItem(){
+    return screenArray[screenArray.length-1].toString();
+}
+
+function add(a,b){
+    return (+a)+(+b);
+}
+
+function subtract(a,b){
+    return (+a)-(+b);
+}
+
+function divide(a,b){
+    if ((+b)===0){    
+    screenArray = [""];
+    setScreen();
+    screen.textContent = "Well Poopies...";
+    }else{
+    return (+a)/(+b);
+    };
+}
+
+function multiply(a,b){
+    return (+a)*(+b);
+}
